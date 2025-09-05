189163b44f1475ea3d9fa5ef959a3151b21b6a1f
Cristhian Joel <u20201b914@upc.edu.pe>
2025-09-05 16:43:06 -0500

    feat(product-id): enhance ProductId value object with documentation and new methods

--- ACME.OOP/Procurement/Domain/Model/ValueObjects/ProductId.cs
+++ ACME.OOP/Procurement/Domain/Model/ValueObjects/ProductId.cs
@@ -1,9 +1,17 @@
 namespace ACME.OOP.Procurement.Domain.Model.ValueObjects;
 
+/// <summary>
+/// Value object representing a unique identifier for a product.
+/// </summary>
 public record ProductId
 {
     public Guid Id { get; init; }
     
+    /// <summary>
+    /// Creates a new instance of <see cref="ProductId"/>. with the specified GUID
+    /// </summary>
+    /// <param name="id"></param>
+    /// <exception cref="ArgumentException"></exception>
     public  ProductId(Guid id)
     {
         if (id == Guid.Empty)
@@ -13,5 +21,15 @@ public record ProductId
         Id = id;
     }
     
+    /// <summary>
+    /// Generates a new unique <see cref="ProductId"/>.\ instance with a unique GUID.
+    /// </summary>
+    /// <returns></returns>
     public static ProductId New()=>new(Guid.NewGuid());
+    
+    /// <summary>
+    ///  Returns a string representation of the product ID.
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()=>Id.ToString();
 }
