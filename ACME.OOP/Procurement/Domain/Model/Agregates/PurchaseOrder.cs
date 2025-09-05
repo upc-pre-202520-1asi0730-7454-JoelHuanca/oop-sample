b16ed6b00dee845bbee317eb2ec55a29705b7991
Cristhian Joel <u20201b914@upc.edu.pe>
2025-09-05 16:59:08 -0500

    feat(purchase-order): add PurchaseOrder and PurchaseOrderItem classes with validation and documentation

---
+++ ACME.OOP/Procurement/Domain/Model/Agregates/PurchaseOrder.cs
@@ -0,0 +1,9 @@
+using ACME.OOP.SCM.Domain.Model.ValueObjects;
+
+namespace ACME.OOP.Procurement.Domain.Model.Agregates;
+
+public class PurchaseOrder (string orderNumber, SupplierId supplierId, 
+    DateTime orderDate, string currency)
+{
+    
+}
